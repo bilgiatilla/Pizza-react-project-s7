31653580d038d6021afa2f19e2401370d9330f27
bilgiatilla <atillabilgi.ab@gmail.com>
2026-02-13 03:13:37 +0300

    Styled-components-create

--- src/Components/OrderComponents/HeaderBanner.jsx
+++ src/Components/OrderComponents/HeaderBanner.jsx
@@ -1,10 +1,43 @@
-import './HeaderBanner.css'
+import styled from 'styled-components'
+const Wrapper = styled.div`
+  background-color: #CE2829;
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+const BannerTitle = styled.h1`
+  font-family: "Londrina Solid", cursive;
+  font-size: 64px;
+  color: white;
+  text-align: center;
+  margin: auto;
+`;
+const Breadcrumb = styled.div`
+  position: absolute;
+  bottom: 30px;
+  left: 400px;
+  font-family: "Barlow", sans-serif;
+  font-size: 16px;
+`;
+const HomeText = styled.span`
+  color: #bdbdbd;`
+
+const OrderText = styled.span`
+  color: white;
+  font-weight: 600;
+`;
 
 export function HeaderBanner () {
     return (
-    <>
-    <h1 className='main-banner-text'>Teknolojik Yemekler</h1>
-    <span className='main-text'>Anasayfa</span><span className='create-order-text'>Sipariş Oluştur</span>
-    </>
+      <Wrapper>
+      <BannerTitle>Teknolojik Yemekler</BannerTitle>
+
+      <Breadcrumb>
+        <HomeText>Anasayfa - </HomeText>
+        <OrderText>Sipariş Oluştur</OrderText>
+      </Breadcrumb>
+    </Wrapper>
     )
 }
